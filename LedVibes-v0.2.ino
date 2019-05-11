1506511226f29a7482830042adef80068215977c
Marcelo <47826132+mlunad88@users.noreply.github.com>
2019-05-11 19:48:35 +0200

    Update LedVibes-v0.2.ino

--- LedVibes-v0.2.ino
+++ LedVibes-v0.2.ino
@@ -2,6 +2,23 @@
 #define N 128 //numero de muestras (debe ser múltiplo de 2)
 #define F 1000  //frecuencia de muestreo (debe ser inferior a 10000 por limitaciones del ADC de Arduino
 
+//
+#define PIN_A 2
+#define PIN_B 3
+#define PIN_C 4
+
+#define PIN_D 7
+#define PIN_E 6
+#define PIN_F 5
+
+#define PIN_G 8
+#define PIN_H 9
+#define PIN_I 10
+
+int volumen;
+int salida;
+
+
 arduinoFFT FFT = arduinoFFT();
 
 void setup() {
@@ -20,6 +37,17 @@ void setup() {
     delay(100);
   };
   //
+  
+    //  para ver si se encienden bien los leds(matriz)
+    for(int i=1; i<=6; i++)
+    {
+        encenderLed1(i);
+        delay(100);
+        encenderLed2(i);
+        delay(100);
+        encenderLed3(i);
+        delay(100);
+    }
   
   }
 
@@ -77,5 +105,95 @@ void loop() {
     delay(10);
     //
     
+// Función que pondrá en los estados correctos para encender un LED (HIGH, LOW e INPUT)
+void ponerEstados(int pinHigh, int pinLow, int pinInput)
+{
+    pinMode(pinHigh,OUTPUT);
+    digitalWrite(pinHigh,HIGH);
+    pinMode(pinLow,OUTPUT);
+    digitalWrite(pinLow,LOW);
+    pinMode(pinInput,INPUT);
+}
+ 
+// Esta función se va a encargar de aplicar la lógica dependiendo del LED que queramos encender
+void encenderLed1 (int led_num)
+{
+    switch(led_num)
+    {
+        case 1:
+            ponerEstados(PIN_A,PIN_B,PIN_C);
+            break;
+        case 2:
+            ponerEstados(PIN_B,PIN_A,PIN_C);
+            break;
+        case 3:
+            ponerEstados(PIN_B,PIN_C,PIN_A);
+            break;
+        case 4:
+            ponerEstados(PIN_C,PIN_B,PIN_A);
+            break;
+        case 5:
+            ponerEstados(PIN_A,PIN_C,PIN_B);
+            break;
+        case 6:
+            ponerEstados(PIN_C,PIN_A,PIN_B);
+            break;   
+        
+    }
+    
+}
+void encenderLed2(int led_num)
+{
+    switch(led_num)
+    {
+        case 1:
+            ponerEstados(PIN_D,PIN_E,PIN_F);
+            break;
+        case 2:
+            ponerEstados(PIN_E,PIN_D,PIN_F);
+            break;
+        case 3:
+            ponerEstados(PIN_E,PIN_F,PIN_D);
+            break;
+        case 4:
+            ponerEstados(PIN_F,PIN_E,PIN_D);
+            break;
+        case 5:
+            ponerEstados(PIN_D,PIN_F,PIN_E);
+            break;
+        case 6:
+            ponerEstados(PIN_F,PIN_D,PIN_E);
+            break;   
+        
+    }
+    
+}
+void encenderLed3(int led_num)
+{
+    switch(led_num)
+    {
+        case 1:
+            ponerEstados(PIN_G,PIN_H,PIN_I);
+            break;
+        case 2:
+            ponerEstados(PIN_H,PIN_G,PIN_I);
+            break;
+        case 3:
+            ponerEstados(PIN_H,PIN_I,PIN_G);
+            break;
+        case 4:
+            ponerEstados(PIN_I,PIN_H,PIN_G);
+            break;
+        case 5:
+            ponerEstados(PIN_G,PIN_I,PIN_H);
+            break;
+        case 6:
+            ponerEstados(PIN_I,PIN_G,PIN_H);
+            break;   
+        
+    }
+    
+}
+ 
     
 }
